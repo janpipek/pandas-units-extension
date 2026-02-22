f601b0df0baf549beef7fee735792df68c914242
Julian Harbeck <harbeck@tu-berlin.de>
2026-02-22 04:26:34 +0100

    Rework `__getitem__` and `__setitem__` functions - Introduce `_simple_new` function to create `UnitsExtensionArray` directly from values and `UnitsDtype` - Outsource index conversion in `__getitem__` and `__setitem__` to pandas  `check_array_indexer` function - Return early for empty lists and `None` in `__setitem__` as it is a no-op - Drop overwriting `test_getitem_mask_raises` and `test_setitem_mask_raises` as messages of `IndexError` are now fitting again after adoption of `check_array_indexer`

--- tests/test_pandas_units_extension.py
+++ tests/test_pandas_units_extension.py
@@ -208,17 +208,6 @@ class TestGroupBy(base.BaseGroupbyTests):
 
 
 class TestGetitem(base.BaseGetitemTests):
-    def test_getitem_mask_raises(self, data):
-        # Overwritten from base class due to different message format for np.array vs pd.array boolean indexing
-        mask = np.array([True, False])
-        msg = f"boolean index did not match indexed array along axis 0; size of axis is {len(data)} but size of corresponding boolean axis is 2"
-        with pytest.raises(IndexError, match=msg):
-            data[mask]
-
-        mask = pd.array(mask, dtype="boolean")
-        with pytest.raises(IndexError, match=msg):
-            data[mask]
-
     def test_unitless(self):
         series = pd.Series([0, 1, 2], dtype="unit[]")
         new_index = [2, 4]
@@ -306,23 +295,7 @@ class TestReduce(base.BaseReduceTests):
 
 
 class TestSetitem(base.BaseSetitemTests):
-    def test_setitem_mask_raises(self, data, box_in_series):
-        # Overwritten from base class due to different message format for np.array vs pd.array boolean indexing
-        # wrong length
-        mask = np.array([True, False])
-
-        if box_in_series:
-            data = pd.Series(data)
-            msg: str = "wrong length"
-        else:
-            msg: str = f"boolean index did not match indexed array along axis 0; size of axis is {len(data)} but size of corresponding boolean axis is 2"
-        with pytest.raises(IndexError, match=msg):
-            data[mask] = data[0]
-
-        mask = pd.array(mask, dtype="boolean")
-        with pytest.raises(IndexError, match=msg):
-            data[mask] = data[0]
-
+    pass
 
 class TestParsing(base.BaseParsingTests):
     @pytest.mark.parametrize("generic", [False, True])
